d869ab57bb5f4bb60620a18490341123c06d89d6
caio.coelho <caio.coelho@totvs.com.br>
2025-07-22 22:14:15 -0300

    basic

---
+++ src/main/java/com/fiap/pj/gerenciador_oficina_core/GerenciadorOficinaCoreApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.pj.gerenciador_oficina_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorOficinaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorOficinaCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/pj/gerenciador_oficina_core/GerenciadorOficinaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.pj.gerenciador_oficina_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorOficinaCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

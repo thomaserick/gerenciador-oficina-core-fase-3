6290f091d16ed30e2a5c760e9e4c72f03a87f813
Vanessa Gomes de Oliveira <assenavsp@hotmail.com>
2025-08-04 16:52:24 -0300

    fix: ajuste no ListarPecaInsumoUseCase

--- src/main/java/com/fiap/pj/core/pecaInsumo/usecase/ListarPecaInsumoUseCase.java
+++ src/main/java/com/fiap/pj/core/pecaInsumo/usecase/ListarPecaInsumoUseCase.java
@@ -1,9 +1,10 @@
 package com.fiap.pj.core.pecaInsumo.usecase;
 
 import com.fiap.pj.core.pecaInsumo.adapter.in.api.request.ListarPecaInsumoRequest;
+import com.fiap.pj.core.pecaInsumo.adapter.in.api.response.PecaInsumoResponse;
 import com.fiap.pj.infra.api.Slice;
 
 public interface ListarPecaInsumoUseCase {
 
-    Slice<com.fiap.pj.core.pecaInsumo.adapter.in.api.response.PecaInsumoResponse> handle(ListarPecaInsumoRequest filterRequest);
+    Slice<PecaInsumoResponse> handle(ListarPecaInsumoRequest filterRequest);
 } 

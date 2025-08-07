b14f086b8a6f6f7fe2a23169e9071e315ae2ea33
Thomas Erick <thomas.oliveira@totvs.com.br>
2025-08-06 21:22:26 -0300

    fix: Ajuste deprecated JWT

--- src/main/java/com/fiap/pj/infra/helpers/JwtUtil.java
+++ src/main/java/com/fiap/pj/infra/helpers/JwtUtil.java
@@ -1,11 +1,14 @@
 package com.fiap.pj.infra.helpers;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -31,8 +34,10 @@ public class JwtUtil {
     }
 
     private Claims extractAllClaims(String token) {
-        return Jwts.parser()
-                .setSigningKey(secret)
+        JwtParser parser = Jwts.parserBuilder()
+                .setSigningKey(buildKey())
+                .build();
+        return parser
                 .parseClaimsJws(token)
                 .getBody();
     }
@@ -52,12 +57,16 @@ public class JwtUtil {
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 horas
-                .signWith(SignatureAlgorithm.HS256, secret)
+                .signWith(buildKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
     public boolean validateToken(String token, String username) {
         final String usernameFromToken = extractUsername(token);
         return (usernameFromToken.equals(username) && !isTokenExpired(token));
+    }
+
+    private Key buildKey() {
+        return Keys.hmacShaKeyFor(this.secret.getBytes());
     }
 }

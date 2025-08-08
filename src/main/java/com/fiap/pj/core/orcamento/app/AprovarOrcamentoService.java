0e6df6981bf448ea6bb71e052292e7bce9a81d3b
Thomas Erick <thomas.oliveira@totvs.com.br>
2025-08-08 16:18:51 -0300

    feat: Criar teste listar orcamento

--- src/main/java/com/fiap/pj/core/orcamento/app/AprovarOrcamentoService.java
+++ src/main/java/com/fiap/pj/core/orcamento/app/AprovarOrcamentoService.java
@@ -7,7 +7,6 @@ import com.fiap.pj.core.orcamento.usecase.command.AprovarOrcamentoCommand;
 import com.fiap.pj.core.ordemservico.domain.OrdemServico;
 import com.fiap.pj.core.ordemservico.domain.OrdemServicoDomainRepository;
 import com.fiap.pj.core.util.security.SecurityContextUtils;
-import com.fiap.pj.infra.security.UserDetailsServiceImpl;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,7 +20,6 @@ public class AprovarOrcamentoService implements AprovarOrcamentoUseCase {
 
     private final OrcamentoDomainRepository repository;
     private final OrdemServicoDomainRepository ordemServicoRepository;
-    private final UserDetailsServiceImpl userDetailsService;
 
     @Override
     public void handle(AprovarOrcamentoCommand cmd) {

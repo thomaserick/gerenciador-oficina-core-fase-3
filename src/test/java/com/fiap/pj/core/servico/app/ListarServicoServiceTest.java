8edf4bf355f45322eaed1203d6c3bf7630c471a6
Thomas Erick <thomas.oliveira@totvs.com.br>
2025-08-12 15:40:09 -0300

    feat: Ajuste pom exclusion covarage package infra

---
+++ src/test/java/com/fiap/pj/core/servico/app/ListarServicoServiceTest.java
@@ -0,0 +1,46 @@
+package com.fiap.pj.core.servico.app;
+
+import com.fiap.pj.core.servico.adapter.in.api.request.ListarServicoRequest;
+import com.fiap.pj.core.servico.adapter.in.api.response.ServicoResponse;
+import com.fiap.pj.core.servico.adapter.out.db.ServicoRepositoryJpa;
+import com.fiap.pj.infra.api.Slice;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ListarServicoServiceTest {
+
+    @Mock
+    private ServicoRepositoryJpa servicoRepositoryJpa;
+
+    @InjectMocks
+    private ListarServicoService listarServicoService;
+
+    @Test
+    void deveListarPecasInsumos() {
+        var request = new ListarServicoRequest("Troca Óleo", PageRequest.of(0, 10), null);
+
+        var slice = new Slice<ServicoResponse>(false, List.of());
+
+        when(servicoRepositoryJpa.findProjectedBy(any(Specification.class), any(PageRequest.class), any(Class.class)))
+                .thenReturn(slice);
+
+        var result = listarServicoService.handle(request);
+
+        assertNotNull(result);
+        assertEquals(0, result.getItems().size());
+    }
+
+} 

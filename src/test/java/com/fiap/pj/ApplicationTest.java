753f7b2ad026a8a7b415f1b1515a7d412445d3a5
Thomas Erick <thomas.oliveira@totvs.com.br>
2025-08-12 16:04:31 -0300

    fix: removido class ApplicationTest

--- src/test/java/com/fiap/pj/ApplicationTest.java
+++ src/test/java/com/fiap/pj/ApplicationTest.java
@@ -2,8 +2,10 @@ package com.fiap.pj;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({"it"})
 class ApplicationTest {
 
     @Test

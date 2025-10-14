f624841bb13c7968dd95dec36ec1f9327e4ad6c2
caio.coelho <caio.coelho@totvs.com.br>
2025-10-13 21:26:37 -0300

    feat: troca secret pipeline

--- src/main/java/com/fiap/pj/infra/externo/orcamento/controller/OrcamentoExternoController.java
+++ src/main/java/com/fiap/pj/infra/externo/orcamento/controller/OrcamentoExternoController.java
@@ -24,7 +24,6 @@ public class OrcamentoExternoController implements OrcamentoExternoControllerOpe
     private final AprovarOrcamentoExternoUseCase aprovarOrcamentoExternoUseCase;
     private final ReprovarOrcamentoExternoUseCase reprovarOrcamentoExternoUseCase;
 
-
     @PostMapping("/{id}/aprovar")
     public ResponseEntity<Void> aprovarOrcamento(@PathVariable UUID id) {
         this.aprovarOrcamentoExternoUseCase.handle(id);
